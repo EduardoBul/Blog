bfcbcb7c53048b28b42a4097f6b6f3f978735672
EduardoBul <carilao.bulnes@gmail.com>
2023-07-12 12:09:46 -0400

    class container a yield

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   #devise_for :users 
 
   devise_for :users, controllers: {
-sessions: 'users/sessions',
-registrations: 'users/registrations'
+  sessions: 'users/sessions',
+  registrations: 'users/registrations' 
 }
   #get '/posts', to: 'posts#index', as: 'user_root'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
